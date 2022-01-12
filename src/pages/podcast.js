855c42a6d3c80a722bf43907b96e69baa279ff4c
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-01-12 18:26:55 +0100

    netlify setup and podcast page

---
+++ src/pages/podcast.js
@@ -0,0 +1,12 @@
+import * as REact from 'react'
+import Layout from '../components/layout'
+
+const Podcast = () => {
+    return(
+        <Layout>
+            <main></main>
+        </Layout>
+    )
+}
+
+export default Podcast
